1b32ff895991b1f9f3d2f4c18902002aa9363bf3
Konrad Jagielski <Konrad.Jagielski@sabre.com>
2017-12-02 17:54:07 +0100

    Added data generator

--- Dragon/Database/Database.cs
+++ Dragon/Database/Database.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using Dragon.DTO;
 
@@ -19,6 +20,7 @@ namespace Dragon.Database
             Routes = new List<Route>();
             Users = new List<User>();
             _userIdCnt = 0;
+            Generate();
         }
 
         public static int GetUserIdCnt()
@@ -70,6 +72,80 @@ namespace Dragon.Database
         public static Journey GetJourneyById(int journeyId)
         {
             return Journeys.First(journey => journey.JourneyId == journeyId);
+        }
+
+        private static void Generate()
+        {
+            ///////////////////USERS
+            Users.Add(new User
+            {
+                Name = "Janek",
+                UserId = 1
+            });
+            Users.Add(new User
+            {
+                Name = "Józek",
+                UserId = 2
+            });
+            Users.Add(new User
+            {
+                Name = "Ksawery",
+                UserId = 3
+            });
+            Users.Add(new User
+            {
+                Name = "Bartek",
+                UserId = 4
+            });
+            Users.Add(new User
+            {
+                Name = "Franek",
+                UserId = 5
+            });
+            ///////////////////ROUTES
+            Routes.Add(new Route
+            {
+                RouteId = 1,
+                Capacity = 2,
+                StartPosition = new Position { PositionX = "2", PositionY = "2" },
+                EndPosition = new Position { PositionX = "4", PositionY = "4" }
+            });
+            Routes.Add(new Route
+            {
+                RouteId = 2,
+                Capacity = 4,
+                StartPosition = new Position { PositionX = "57", PositionY = "45" },
+                EndPosition = new Position { PositionX = "46", PositionY = "78" }
+            });
+            Routes.Add(new Route
+            {
+                RouteId = 3,
+                Capacity = 3,
+                StartPosition = new Position { PositionX = "23", PositionY = "26" },
+                EndPosition = new Position { PositionX = "44", PositionY = "44" }
+            });
+            ///////////////////JOURNEYS
+            Journeys.Add(new Journey
+            {
+                IsDriver = true,
+                JourneyId = 1,
+                RouteId = 1,
+                UsersIds = new List<int>() {2, 4}
+            });
+            Journeys.Add(new Journey
+            {
+                IsDriver = true,
+                JourneyId = 2,
+                RouteId = 3,
+                UsersIds = new List<int>() { 1, 5, 3 }
+            });
+            Journeys.Add(new Journey
+            {
+                IsDriver = true,
+                JourneyId = 3,
+                RouteId = 2,
+                UsersIds = new List<int>() { 1, 2, 4 }
+            });
         }
     }
 }
